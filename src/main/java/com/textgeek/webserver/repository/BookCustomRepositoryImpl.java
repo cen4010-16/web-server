38fed0fbeb8217bd21c15adadab43afacdeb3160
Pedro Prieto <pedro.prieto003@gmail.com>
2022-01-29 18:37:54 -0500

    Added limit and offset to get Books endpoint

--- src/main/java/com/textgeek/webserver/repository/BookCustomRepositoryImpl.java
+++ src/main/java/com/textgeek/webserver/repository/BookCustomRepositoryImpl.java
@@ -21,10 +21,22 @@ public class BookCustomRepositoryImpl implements BookCustomRepository {
         Root<Book> root = query.from(Book.class);
         query.select(root);
 
+        int offset = 0;
+        int limit = 25;
+
         if (allParams.containsKey("genre")) {
             query.where(qb.equal(root.get("genre"), allParams.get("genre")));
         }
 
-        return entityManager.createQuery(query).getResultList();
+        if (allParams.containsKey("offset")) {
+            offset = Integer.valueOf(allParams.get("offset")) - 1;
+        }
+
+        if (allParams.containsKey("limit")) {
+            limit = Integer.valueOf(allParams.get("limit"));
+        }
+
+        return entityManager.createQuery(query).setFirstResult(offset).setMaxResults(limit)
+            .getResultList();
     }
 }
