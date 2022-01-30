14f72d76bc116af9067b3695aaf289cc60118e77
Pedro Prieto <pedro.prieto003@gmail.com>
2022-01-30 16:32:55 -0500

    Format Code

--- sql/textgeek/migrations/V1__Initial_tables.sql
+++ sql/textgeek/migrations/V1__Initial_tables.sql
@@ -5,9 +5,9 @@ CREATE TABLE author
 
 CREATE TABLE book
 (
-    id BIGSERIAL PRIMARY KEY NOT NULL,
+    id          BIGSERIAL PRIMARY KEY NOT NULL,
     copies_sold INTEGER DEFAULT 0,
-    genre VARCHAR(30)
+    genre       VARCHAR(30)
 );
 CREATE TABLE profile
 (
@@ -15,9 +15,9 @@ CREATE TABLE profile
 );
 CREATE TABLE book_rating
 (
-    book_id BIGSERIAL NOT NULL,
+    book_id    BIGSERIAL NOT NULL,
     profile_id BIGSERIAL NOT NULL,
-    rating INTEGER,
+    rating     INTEGER,
     CONSTRAINT FK_Book_Rating FOREIGN KEY (book_id) REFERENCES book (id),
     CONSTRAINT FK_Profile_Rating FOREIGN KEY (profile_id) REFERENCES profile (id)
 );
@@ -31,7 +31,7 @@ CREATE TABLE credit_card
 );
 CREATE TABLE user_credit_card
 (
-    card_id BIGSERIAL NOT NULL,
+    card_id    BIGSERIAL NOT NULL,
     profile_id BIGSERIAL NOT NULL,
     CONSTRAINT FK_Profile_Card FOREIGN KEY (profile_id) REFERENCES profile (id),
     CONSTRAINT FK_Card_Card FOREIGN KEY (card_id) REFERENCES credit_card (id)
@@ -43,7 +43,7 @@ CREATE TABLE shopping_cart
 CREATE TABLE shopping_cart_books
 (
     shopping_cart_id BIGSERIAL NOT NULL,
-    book_id BIGSERIAL NOT NULL,
+    book_id          BIGSERIAL NOT NULL,
     CONSTRAINT FK_Shopping_Cart FOREIGN KEY (shopping_cart_id) REFERENCES shopping_cart (id),
     CONSTRAINT FK_Shopping_Cart_Book FOREIGN KEY (book_id) REFERENCES book (id)
 );
@@ -54,32 +54,56 @@ CREATE TABLE wishlist
 CREATE TABLE wishlist_books
 (
     wishlist_id BIGSERIAL NOT NULL,
-    book_id BIGSERIAL NOT NULL,
+    book_id     BIGSERIAL NOT NULL,
     CONSTRAINT FK_Wishlist FOREIGN KEY (wishlist_id) REFERENCES wishlist (id),
     CONSTRAINT FK_Wishlist_Book FOREIGN KEY (book_id) REFERENCES book (id)
 );
 
-INSERT INTO book (copies_sold, genre) VALUES (15, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (100, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (55, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (67, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (324, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (347, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (34, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (75, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (85, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (43, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (23, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (2, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (954, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (346, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (357, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (235, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (963, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (734, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (246, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (163, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (78, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (95, 'romance');
-INSERT INTO book (copies_sold, genre) VALUES (43, 'fantasy');
-INSERT INTO book (copies_sold, genre) VALUES (75, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (15, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (100, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (55, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (67, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (324, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (347, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (34, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (75, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (85, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (43, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (23, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (2, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (954, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (346, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (357, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (235, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (963, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (734, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (246, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (163, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (78, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (95, 'romance');
+INSERT INTO book (copies_sold, genre)
+VALUES (43, 'fantasy');
+INSERT INTO book (copies_sold, genre)
+VALUES (75, 'romance');
--- src/main/java/com/textgeek/webserver/service/BookService.java
+++ src/main/java/com/textgeek/webserver/service/BookService.java
@@ -4,10 +4,6 @@ import com.textgeek.webserver.model.Book;
 import com.textgeek.webserver.repository.BookRepository;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
-import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
