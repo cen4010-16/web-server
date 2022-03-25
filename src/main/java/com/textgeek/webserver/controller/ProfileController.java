e6389d59e1a8cac5acfecedc49c1a7acb01c7870
Josue Daniel Prendes Diaz <jpren016@fiu.edu>
2022-03-25 12:31:13 -0700

    Updated the Profile model, created ProfileController and ProfileRepository. Postman returned the created profile successfully.

---
+++ src/main/java/com/textgeek/webserver/controller/ProfileController.java
@@ -0,0 +1,22 @@
+package com.textgeek.webserver.controller;
+
+import com.textgeek.webserver.model.Profile;
+import com.textgeek.webserver.repository.ProfileRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1")
+public class ProfileController {
+    @Autowired
+    private ProfileRepository profileRepository;
+
+    @PostMapping("/profiles")
+    public Profile addProfile(@RequestBody Profile profile){
+        System.out.println(profile);
+        return profileRepository.save(profile);
+    }
+}
